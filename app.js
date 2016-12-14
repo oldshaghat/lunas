05c85b9ff3546a06b1e0b67db03708afa7098d74
oldshaghat <oldshaghat@gmail.com>
2016-12-13 23:41:55 -0500

    trying express - now with server?

--- app.js
+++ app.js
@@ -20,6 +20,12 @@ app.get('/health', function(req, res) {
     res.sendStatus(200);
 });
 
+var server = http.createServer(app);
+
+server.listen(env.NODE_PORT || 3000, env.NODE_IP || 'localhost', function () {
+  console.log(`Application worker ${process.pid} started...`);
+});
+
 /*
 let server = http.createServer(function (req, res) {
   let url = req.url;
